af5964f736994193ac8b8741159661c6fb2afbfb
Zoe Long <zoelong@sfs-wifi-dhcp-10-143-121-100.wifi.sfsu.edu>
2024-09-19 12:02:52 -0700

    modifyFork

--- js/starter/hello.js
+++ js/starter/hello.js
@@ -1,1 +1,5 @@
-console.log("hello world")
+const sumEven = (arr) => {
+  return arr.filter((num) => num % 2 === 0).reduce((acc, num) => acc + num, 0);
+};
+
+console.log(sumEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 30
